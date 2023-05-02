8a7e0fc57e13de337dcc285786148eb7c2f07178
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-05-02 23:00:40 +0530

    roboshop

--- services.tf
+++ services.tf
@@ -63,7 +63,7 @@ data "aws_security_group" "allow-all" {
 data "aws_ami" "centos" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
-  owners           = "973714476881"
+  owners           = ["973714476881"]
 }
 
 
