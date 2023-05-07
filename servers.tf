27364685ab9be92d71c461f9a2c78bc08c30b85d
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-05-07 11:23:10 +0530

    roboshop

---
+++ servers.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+  vpc_id      = data.aws_security_group.allow-all
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+data "aws_security_group" "allow-all" {
+  id = "allow-all"
+  }
+
+
+resource "aws_route53_record" "records" {
+  zone_id = "Z08045122E2EQN1OR1WS6"
+  name    = "frontend-dev.pavan345.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
