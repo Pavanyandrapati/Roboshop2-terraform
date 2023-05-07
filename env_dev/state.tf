51dc56d256ee331c427b288642f85bc51cc8ea91
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-05-07 12:19:29 +0530

    roboshop

---
+++ env_dev/state.tf
@@ -0,0 +1,3 @@
+bucket = "roboshop2-terraform"
+key    = "/home/centos/Roboshop2-terraform"
+region = "us-east-1"
--- servers.tf
+++ servers.tf
@@ -1,22 +1,20 @@
-resource "aws_instance" "frontend" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = "t3.micro"
+resource "aws_instance" "instance" {
+  for_each = var.components
+  ami           = "data.aws_ami.centos.image_id"
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  instance_type = each.value["instance_type"]
 
   tags = {
-    Name = "frontend"
+    Name = "each.value["name"]
   }
 }
 
-data "aws_security_group" "allow-all" {
-  name = "allow-all"
-  }
-
 
 resource "aws_route53_record" "records" {
+  for_each = var.components
   zone_id = "Z08045122E2EQN1OR1WS6"
-  name    = "frontend-dev.pavan345.online"
+  name    = "${each.value["name"]}-dev.pavan345.online"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.frontend.private_ip]
+  records = [aws_instance.instance[each.value["name"]].private_ip]
 }
