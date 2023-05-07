22cd8aeacae3d4469453769678bd124bef485e07
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-05-07 19:08:54 +0530

    roboshop

--- env_dev/state.tf
+++
@@ -1,3 +0,0 @@
-bucket = "roboshop2-terraform"
-key    = "/home/centos/Roboshop2-terraform"
-region = "us-east-1"
--- servers.tf
+++ servers.tf
@@ -1,20 +1,28 @@
-resource "aws_instance" "instance" {
-  for_each = var.components
-  ami           = "data.aws_ami.centos.image_id"
-  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
-  instance_type = each.value["instance_type"]
+resource "aws_instance" "frontend" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [data.aws_security_groups.allow-all.id]
 
   tags = {
-    Name = each.value["name"]
+    Name = "frontend"
   }
 }
 
-
-resource "aws_route53_record" "records" {
-  for_each = var.components
+resource "aws_route53_record" "frontend" {
   zone_id = "Z08045122E2EQN1OR1WS6"
-  name    = "${each.value["name"]}-dev.pavan345.online"
+  name    = "frontend-dev.pavan345.online"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.instance[each.value["name"]].private_ip]
+  records = [aws_instance.frontend.private_ip]
 }
+
+data "aws_security_groups" "allow-all" {
+  name = "allow-all"
+}
+
+data "aws_ami" "centos" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
