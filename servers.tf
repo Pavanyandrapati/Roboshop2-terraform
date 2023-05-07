ad0bb4e2d35678eb88198050fcf4a55b4d8667bc
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-05-07 19:29:04 +0530

    roboshop

--- servers.tf
+++ servers.tf
@@ -1,20 +1,24 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "instance" {
+  count = length(var.components)
   ami = "data.aws_ami.centos.image.id"
   instance_type = "t3.micro"
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
-    Name = "frontend"
+    Name = "var.components[count.index]"
   }
 }
-
-resource "aws_route53_record" "frontend" {
-  zone_id = "Z08045122E2EQN1OR1WS6"
-  name    = "frontend-dev.pavan345.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend.private_ip]
+variable "components" {
+  default = [frontend,mongodb,cart]
 }
+
+#resource "aws_route53_record" "frontend" {
+#  zone_id = "Z08045122E2EQN1OR1WS6"
+#  name    = "${}-dev.pavan345.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.instance[count.index].private_ip]
+#}
 
 data "aws_security_group" "allow-all" {
   name = "allow-all"
